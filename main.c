b926f30c77923c81084499c83a6628e38b9bd556
Cem PEKGÖZ <cem.pekgoz@fsm.edu.tr>
2026-01-06 12:52:33 +0300

    Initial Pokemon game project

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
 #include "structs.h"
 
-Type Types[TYPE_CO];
-Move Moves[MOVE_CO];
-Pokemon Pokemons[POKE_CO];
-Player Player1, Player2;
 
 int main() {
+    
+    Type *Types = malloc(sizeof(Type) * TYPE_CO);
+    Move *Moves = malloc(sizeof(Move) * MOVE_CO);
+    Pokemon *Pokemons = malloc(sizeof(Pokemon) * POKE_CO);
+
+    Player Player1, Player2;
+
+    if (!Types || !Moves || !Pokemons) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
     init(Types, Moves, Pokemons, &Player1, &Player2);
-    game(&Player1, &Player2, Types); 
+    game(&Player1, &Player2, Types);
+
+    free(Types);
+    free(Moves);
+    free(Pokemons);
+
     return 0;
 }
+
